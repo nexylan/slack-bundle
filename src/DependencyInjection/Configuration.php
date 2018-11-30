2b707e6d42f7fb251622e8aea143781f52223376
Michael Babker <michael.babker@gmail.com>
2018-11-30 13:11:37 -0600

    Update bundle configuration for Symfony 4.2 change

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,8 +23,14 @@ final class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder(): TreeBuilder
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('nexy_slack');
+        $treeBuilder = new TreeBuilder('nexy_slack');
+
+        if (method_exists($treeBuilder, 'getRootNode')) {
+            $rootNode = $treeBuilder->getRootNode();
+        } else {
+            // BC layer for symfony/config 4.1 and older
+            $rootNode = $treeBuilder->root('nexy_slack');
+        }
 
         $rootNode
             ->children()
