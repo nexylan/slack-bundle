0b801182293eb45481fee9070c7194c82d67b73a
Nicklas Sjöqvist <nicklas.sjoqvist@aloq.se>
2019-08-10 18:42:05 +0200

    remove class from test

--- tests/DependencyInjection/NexySlackExtensionTest.php
+++ tests/DependencyInjection/NexySlackExtensionTest.php
@@ -47,7 +47,7 @@ class NexySlackExtensionTest extends AbstractExtensionTestCase
         $this->assertContainerBuilderHasParameter('nexy_slack.endpoint', $endpoint);
         $this->assertContainerBuilderHasParameter('nexy_slack.config', $slackConfig);
 
-        $this->assertContainerBuilderHasService('Nexy\Slack\Client', Client::class);
+        $this->assertContainerBuilderHasService('Nexy\Slack\Client');
 
         $this->assertContainerBuilderHasServiceDefinitionWithArgument('Nexy\Slack\Client', 0, '%nexy_slack.endpoint%');
         $this->assertContainerBuilderHasServiceDefinitionWithArgument('Nexy\Slack\Client', 1, '%nexy_slack.config%');
